6194a911f254aac353edd8ddeeea777438185500
tursom <tursom@foxmail.com>
2021-06-18 20:10:57 +0800

    update

--- test/ngx_list_test.c
+++ test/ngx_list_test.c
@@ -11,10 +11,10 @@ int main(int argc, char **argv) {
     ngx_pool_t *pool = ngx_create_pool(4096, NULL);
     ngx_list_t *list = ngx_list_create(pool, 10, sizeof(ngx_str_t));
 
-    for (int i = 0; i < 32; i++) {
+    for (int i = 0; i < 128; i++) {
         ngx_str_t *str = ngx_list_push(list);
         char *buf = ngx_palloc(pool, 32);
-        sprintf(buf, "hello %d", i);
+        sprintf(buf, "hello %d", i + 1);
         str->len = strlen(buf);
         str->data = (u_char *) buf;
     }
